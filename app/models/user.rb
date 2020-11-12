319483baa382f79a42a8a06d4aed12ef9cf3838b
thaiduongtran98 <thaiduongtran98@gmail.com>
2020-11-12 16:43:27 +0800

    Chapter 8 : Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,19 @@ class User < ApplicationRecord
                     minimum: Settings.validates.user.min_length_pass_user
                   }
 
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
+
   private
 
   def downcase_email
