be93898244a2df715ddb08da5964065810b4bb62
thaiduongtran98 <thaiduongtran98@gmail.com>
2020-11-13 16:55:41 +0800

    Chapter 10 : UPDATING, SHOWING, AND DELETING USERS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new create show)
+  before_action :find_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
   def index
     @users = User.page(params[:page]).per Settings.pagination.users_per_page
   end
@@ -7,12 +12,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
+  def show; end
 
-    flash[:danger] = t "user_not_found"
-    redirect_to users_path
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "profile_updated"
+      redirect_to @user
+    else
+      flash[:danger] = t "profile_update_failed"
+      render :edit
+    end
   end
 
   def create
@@ -27,9 +38,48 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    if @user.destroy
+      flash[:success] = t "user_deleted"
+    else
+      flash[:danger] = t "something_wrong"
+    end
+    redirect_to users_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit User::USER_PERMIT
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "please_log_in"
+    redirect_to login_url
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:danger] = t "something_wrong"
+    redirect_to root_path
+  end
+
+  def admin_user
+    return if current_user.admin?
+
+    flash[:danger] = t "you_dont_have_permision"
+    redirect_to root_path
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = "something_wrong"
+    redirect_to root_path
   end
 end
