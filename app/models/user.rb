8b25588ddcf72cc67928b44cdbb2904f024e7b17
thaiduongtran98 <thaiduongtran98@gmail.com>
2020-11-13 11:47:28 +0800

    Chapter 9 : Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   before_save :downcase_email
+
+  attr_accessor :remember_token
+
   USER_PERMIT = %i(name email password password_confirmation).freeze
 
   validates :name, presence: true,
@@ -29,6 +32,23 @@ class User < ApplicationRecord
         end
       BCrypt::Password.create(string, cost: cost)
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_054015) do
+ActiveRecord::Schema.define(version: 2020_11_13_000447) do
 
   create_table "microsts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_054015) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
